7b7e8d6d0302616df85045042ad8b0061e70a15e
Vincent <bissonnette156@gmail.com>
2025-10-18 21:47:26 -0400

    First commit

---
+++ SolarSystem/SolarSystem.cpp
@@ -0,0 +1,35 @@
+﻿// SolarSystem.cpp : définit le point d'entrée de l'application.
+//
+
+#include "SolarSystem.h"
+#include "PxPhysicsAPI.h"
+
+using namespace std;
+using namespace physx;
+
+static PxDefaultAllocator		gAllocator;
+static PxDefaultErrorCallback	gErrorCallback;
+
+int main()
+{
+	auto gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+
+	auto gPvd = PxCreatePvd(*gFoundation);
+	PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+	gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+
+	auto gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd);
+
+	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+	auto gDispatcher = PxDefaultCpuDispatcherCreate(2);
+	sceneDesc.cpuDispatcher = gDispatcher;
+	sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+	auto gScene = gPhysics->createScene(sceneDesc);
+
+	gScene->simulate(1.0f / 60.0f);
+	gScene->fetchResults(true);
+
+	cout << "Hello CMake." << endl;
+	return 0;
+}
